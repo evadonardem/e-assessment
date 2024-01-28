a809d176242f592a0c1dbd94920ed58ac39f3450
Dave Medrano <evadonardem@gmail.com>
2024-01-29 00:23:00 +0800

    Question types model, factory, and seeder.

---
+++ database/seeders/QuestionTypeSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\QuestionType;
+use Illuminate\Database\Seeder;
+
+class QuestionTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $questionTypes = [
+            [
+                'code' => 'MCQ',
+                'description' => 'Multiple Choice Questions',
+            ],
+            [
+                'code' => 'ARQ',
+                'description' => 'Alternate Response Questions',
+            ],
+        ];
+
+        $model = new QuestionType();
+        foreach ($questionTypes as $questionType) {
+            $isExist = $model->newQuery()
+                ->where('code', $questionType['code'])
+                ->count() > 0;
+
+            if (! $isExist) {
+                $model->newQuery()
+                    ->create($questionType);
+            }
+        }
+    }
+}
