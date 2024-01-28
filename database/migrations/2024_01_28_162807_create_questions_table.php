59ebac1f09a5d15d404b5fd98143d8d01b4505bc
Dave Medrano <evadonardem@gmail.com>
2024-01-29 01:36:00 +0800

    Questions migration, model, factory, and inital endpoint.

---
+++ database/migrations/2024_01_28_162807_create_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\QuestionType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->text('description');
+            $table->foreignIdFor(QuestionType::class)
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+            $table->boolean('is_random_options')->default(false);
+            $table->boolean('is_published')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Question;
+use App\Models\QuestionType;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        Question::create([
+            'description' => 'Test MCQ',
+            'question_type_id' => QuestionType::where('code', 'MCQ')->first()->id,
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Question::create([
+            'description' => 'Test ARQ',
+            'question_type_id' => QuestionType::where('code', 'ARQ')->first()->id,
+        ]);
     }
 }
