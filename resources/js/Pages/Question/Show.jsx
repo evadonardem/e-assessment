2623f3bf149dd101cd6e65404dc2d66aacc7666c
Dave Medrano <evadonardem@gmail.com>
2025-02-21 12:35:55 +0800

    Removed tinyMCE package.

--- resources/js/Pages/Question/Show.jsx
+++ resources/js/Pages/Question/Show.jsx
@@ -1,12 +1,9 @@
-import { Editor } from '@tinymce/tinymce-react'
 import Layout from '../Layout'
 
 const Show = () => {
   return (
     <>
       <h1>Question</h1>
-      <Editor
-        apiKey='pm89xh1s4bq3cgrn1232293tudye2x5lddh0siu6z65iflfq'/>
     </>
   );
 };
